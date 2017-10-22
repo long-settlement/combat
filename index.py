86875f99378c2cb380e3953b3ba1a4402194a656
David Langton <djlangton+git@googlemail.com>
2017-10-22 22:08:22 +0000

    Add more info

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'greetings': 'david'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
